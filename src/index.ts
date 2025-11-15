817445f5366011cb8f7b1b2361fc2da3e226ea82
Devyash Lodha <lodhad@gmail.com>
2025-11-14 18:35:49 -0800

    Update index.ts
    Updating the copyright year. Not sure why I typed 2024 instead of 2025 ;)

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@
  * 
  * @license MIT
  * 
- * Copyright (c) 2024 Devyash Lodha
+ * Copyright (c) 2025 Devyash Lodha
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
